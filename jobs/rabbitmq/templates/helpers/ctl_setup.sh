19436b40553e9c0f95ed591271e9a05bf3ef483a
Norman Abramovitz <norm@starkandwayne.com>
2022-04-09 22:49:18 +0000

    added code to use fqdn names

--- jobs/rabbitmq/templates/helpers/ctl_setup.sh
+++ jobs/rabbitmq/templates/helpers/ctl_setup.sh
@@ -75,7 +75,8 @@ chmod 600 /home/vcap/.erlang.cookie
 export PATH=$PATH:/var/vcap/packages/erlang/bin:/var/vcap/packages/rabbitmq/sbin
 export RABBITMQ_HOME=/var/vcap/jobs/rabbitmq
 export RABBITMQ_CONFIG_FILE=$RABBITMQ_HOME/config/rabbitmq
-export RABBITMQ_NODENAME="<%= spec.name %>@<%= spec.id.tr("-","") %>"
+export RABBITMQ_NODENAME="<%= spec.name %>@<%= spec.address %>"
+export RABBITMQ_USE_LONGNAME="true"
 export RABBITMQ_LOG_BASE=/var/vcap/sys/log/rabbitmq
 export RABBITMQ_MNESIA_BASE=/var/vcap/store/rabbitmq
 export RABBITMQ_PID_FILE=$PIDFILE
