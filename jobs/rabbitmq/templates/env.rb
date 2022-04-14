ee0f93e8b33a934308532cdcf62ed908c42a0ecc
Wayne E. Seguin <wayneeseguin@gmail.com>
2022-04-13 21:49:18 -0400

    Fixed PID FILE

--- jobs/rabbitmq/templates/env.rb
+++ jobs/rabbitmq/templates/env.rb
@@ -9,21 +9,19 @@ export TMP_DIR=/var/vcap/sys/tmp/rabbitmq
 export STORE_DIR=/var/vcap/store/rabbitmq
 
 # RabbitMQ
-export RABBITMQ_HOME=/var/vcap/jobs/rabbitmq
+export RABBITMQ_ADMIN_PASS="<%= p('rabbitmq.admin.pass') %>"
+export RABBITMQ_ADMIN_USER="<%= p('rabbitmq.admin.user') %>"
 export RABBITMQ_CONFIG_FILE=$RABBITMQ_HOME/config/rabbitmq
-export RABBITMQ_NODENAME="rabbit@<%= spec.id %>.<%= spec.name %>.blacksmith.<%= spec.deployment %>.bosh"
-export RABBITMQ_USE_LONGNAME="true"
+export RABBITMQ_HOME=/var/vcap/jobs/rabbitmq
 export RABBITMQ_LOG_BASE=/var/vcap/sys/log/rabbitmq
 export RABBITMQ_MNESIA_BASE=/var/vcap/store/rabbitmq
-export RABBITMQ_PID_FILE=$PIDFILE
-export RABBITMQ_ADMIN_USER="<%= p('rabbitmq.admin.user') %>"
-export RABBITMQ_ADMIN_PASS="<%= p('rabbitmq.admin.pass') %>"
+export RABBITMQ_NODENAME="rabbit@<%= spec.id %>.<%= spec.name %>.blacksmith.<%= spec.deployment %>.bosh"
+export RABBITMQ_PID_FILE="$RUN_DIR/rabbitmq.pid"
+export RABBITMQ_USE_LONGNAME="true"
 export RABBITMQ_VHOST="/"
-export RABBITMQ_RUN_DIR=/var/vcap/sys/run/rabbitmq-server
-export RABBITMQ_PID_FILE="$RABBITMQ_RUN_DIR/beam.pid"
-export EPMD_PID_FILE="$RABBITMQ_RUN_DIR/epmd.pid"
 
 # Erlang
+export EPMD_PID_FILE="$RUN_DIR/epmd.pid"
 export ERL_INETRC=/var/vcap/jobs/rabbitmq/config/erl_inetrc
 
 # Elixir expects utf8, locale must be set to UTF-8.
